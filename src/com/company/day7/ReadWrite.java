aad1ae67111ca7193e07b83b5dd40d0a8c25950b
brightboost <info@brightboostonline.nl>
2020-07-08 11:42:03 +0200

    more on read/write

--- src/com/company/day7/ReadWrite.java
+++ src/com/company/day7/ReadWrite.java
@@ -10,9 +10,12 @@ public class ReadWrite {
 
 // Write the content in file
         try {
-            FileWriter fileWriter = new FileWriter(absolutePath);
+            //using append = true, you can append to a file, so not overwriting it
+            FileWriter fileWriter = new FileWriter(absolutePath, true);
             String fileContent = "This could be anything, really.";
-            fileWriter.write(fileContent);
+            //if you want to append, use the .append, else you can use .write
+            //if append is not set to true, .append will just write
+            fileWriter.append(fileContent);
             fileWriter.close();
         } catch (IOException e) {
             // Exception handling
@@ -27,6 +30,10 @@ public class ReadWrite {
         try {
             FileReader fileReader = new FileReader(absolutePath);
             int ch;
+            //end of file character is -1
+            //range of char is between 0 and 65535
+            //read has to give back an int, because end of file doesnt fit in char
+            //this is why we have to cast back the int to a char in our print line
             while((ch = fileReader.read()) != -1) {
                 System.out.print((char)ch);
             }
@@ -39,5 +46,11 @@ public class ReadWrite {
             System.out.println("something else IO like went wrong");
             // Exception handling
         }
+
+        //exercise
+        //what you have just written to your file
+        //you are going to read
+        //and write to another file
+        //what is the result?
     }
 }
